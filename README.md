bf909b4e3d09ee7c8f2324be9f18838cbec93b9e
CulturalGame <alore13l@gmail.com>
2022-04-13 22:19:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cementera_Projecto_Integrados
+ Este es un repositorio para trabajar en el proyecto integrador
---
+++ src/Clases/ConexionMySQL.java
@@ -0,0 +1,24 @@
+
+package Clases;
+import java.sql.*;
+import javax.swing.*;
+
+public class ConexionMySQL {
+    
+    Connection conect = null;
+    
+    public Connection ConexionMySQL()
+    {
+        try 
+        {
+         Class.forName("com.mysql.jdbc.Driver");
+         conect = DriverManager.getConnection("jdbc:mysql://localhost/Cementera","root","Polo76K$");
+         System.out.print("Conexion a Tienda exitosa");
+        } catch (Exception error) {
+         JOptionPane.showMessageDialog(null,"Error en la conxion: " + error);
+         
+        }
+        return conect;
+    }
+    
+}
